357359f3505b685d465d29f7dd9cf034cf4bfd0e
Chris Reynolds <chris@jazzsequence.com>
2024-10-08 22:33:13 -0600

    make the button disappear after the ipsum is generated

--- assets/js/block.js
+++ assets/js/block.js
@@ -1,5 +1,6 @@
 const { registerBlockType } = wp.blocks;
 const { useBlockProps, RichText } = wp.blockEditor;
+const { useState } = wp.element;
 
 const horrorTexts = horrorLipsumData.texts;
 
@@ -17,19 +18,21 @@ registerBlockType('horror-lorem-ipsum/random-paragraph', {
     edit: (props) => {
         const blockProps = useBlockProps();
         const { attributes: { content }, setAttributes } = props;
+        const [generated, setGenerated] = useState(false);
 
         const onChangeContent = (newContent) => {
             setAttributes({ content: newContent });
         };
 
-		const generateRandomText = () => {
-			let randomParagraph = '';
-			for (let i = 0; i < 4; i++) { // Generate 4 sentences to form a paragraph
-				const randomIndex = Math.floor(Math.random() * horrorTexts.length);
-				randomParagraph += horrorTexts[randomIndex] + ' ';
-			}
-			setAttributes({ content: randomParagraph.trim() });
-		};
+        const generateRandomText = () => {
+            let randomParagraph = '';
+            for (let i = 0; i < 4; i++) {
+                const randomIndex = Math.floor(Math.random() * horrorTexts.length);
+                randomParagraph += horrorTexts[randomIndex] + ' ';
+            }
+            setAttributes({ content: randomParagraph.trim() });
+            setGenerated(true);
+        };
 
         return wp.element.createElement(
             'div',
@@ -40,7 +43,7 @@ registerBlockType('horror-lorem-ipsum/random-paragraph', {
                 onChange: onChangeContent,
                 placeholder: 'Click the button to generate a random horror paragraph...',
             }),
-            wp.element.createElement(
+            !generated && wp.element.createElement(
                 'button',
                 {
                     onClick: generateRandomText,
