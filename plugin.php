63c6fe861e6d3228d48c57c0ecb2db8fbd8c2c56
Chris Reynolds <chris@jazzsequence.com>
2024-10-10 23:03:06 -0600

    code style

--- plugin.php
+++ plugin.php
@@ -7,7 +7,7 @@
  * Author: Chris
  */
 
-if (! defined('ABSPATH')) {
+if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
@@ -17,38 +17,36 @@ function horror_ipsum_texts() {
 }
 
 // Register the block script.
-function horror_lorem_ipsum_register_block()
-{
+function horror_ipsum_register_block() {
 	wp_register_script(
-		'horror-lorem-ipsum-block',
-		plugins_url('assets/js/block.js', __FILE__),
-		array('wp-blocks', 'wp-element', 'wp-block-editor'),
-		filemtime(plugin_dir_path(__FILE__) . 'assets/js/block.js')
+		'horror-ipsum-block',
+		plugins_url( 'assets/js/block.js', __FILE__ ),
+		['wp-blocks', 'wp-element', 'wp-block-editor'],
+		filemtime( plugin_dir_path( __FILE__ ) . 'assets/js/block.js' )
 	);
 
 	// Localize the script to pass PHP data to JavaScript.
 	wp_localize_script(
-		'horror-lorem-ipsum-block',
+		'horror-ipsum-block',
 		'horrorLipsumData',
-		array(
-			'texts' => horror_lorem_ipsum_texts(),
-		)
+		[ 'texts' => horror_ipsum_texts() ]
 	);
 
-	register_block_type('horror-lorem-ipsum/random-paragraph', array(
-		'editor_script' => 'horror-lorem-ipsum-block',
-	));
+	register_block_type(
+		'horror-ipsum/random-paragraph', 
+		['editor_script' => 'horror-ipsum-block']
+	);
 }
-add_action('init', 'horror_lorem_ipsum_register_block');
+add_action( 'init', 'horror_ipsum_register_block' );
 
 // Enqueue the CSS for the block.
-function horror_lorem_ipsum_enqueue_styles()
+function horror_ipsum_enqueue_styles()
 {
 	wp_enqueue_style(
-		'horror-lorem-ipsum-style',
-		plugins_url('assets/css/style.css', __FILE__),
-		array(),
-		filemtime(plugin_dir_path(__FILE__) . 'assets/css/style.css')
+		'horror-ipsum-style',
+		plugins_url( 'assets/css/style.css', __FILE__ ),
+		[],
+		filemtime( plugin_dir_path( __FILE__ ) . 'assets/css/style.css' )
 	);
 }
-add_action('enqueue_block_editor_assets', 'horror_lorem_ipsum_enqueue_styles');
+add_action( 'enqueue_block_editor_assets', 'horror_ipsum_enqueue_styles' );
